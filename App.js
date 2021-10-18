9f320b664f8d16cdedfa56e4f70b8c682e7c6914
SaifAlmarfi <87947658+SaifAlmarfi@users.noreply.github.com>
2021-10-18 22:44:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,180 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div className="App">
+    <header className="App-header">
+      <nav className="navbar navbar-expand-lg ">
+       <div className="container">
+        <a className="navbar-brand" href="#">
+          <h3><strong>EDYODA</strong></h3>
+          <p className="logo-stories-text">Stories</p>
+        </a>
+        <span className="navbar-toggler-icon"></span>
+        
+         
+        <div className="collapse navbar-collapse" >
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" >
+                Explore Categories
+              </a>
+              </li>
+              </ul>
+              </div>
+              
+              <div className="nav">
+              <div id="site-info">EdYoda is free learning and knowledge sharing platform for techies </div>
+              </div>
+              <button className="nav1">Go To Main Website</button>
+       </div>
+      </nav>
+    </header> 
+
+              
+
+              <section class="heading-trending">
+              <h1 className="heading">Latest Posts</h1>
+              <div className="categories-container-">
+                <i className="fa fa-filter"></i>
+                <span className="filter-text"> Filter by Category</span>
+          
+                <div className="categories-list">
+                  <a className="tags all" href="">All</a>
+                  <a className="tags" href="#">Artificial Intelligence</a>
+                  <a className="tags" href="#">Cloud Computing</a>
+                  <a className="tags" href="#">DevOps</a>
+                  <a className="tags" href="#">Programming Languages</a>
+                  <a className="tags" href="#">Mobile Application Development</a>
+                  <a className="tags" href="#">Technology And Tools</a>
+                  <a className="tags" href="#">Get a job in a Tech Company</a>
+                  <a className="tags" href="#">Others</a>
+                </div>
+              </div>
+            </section>
+             
+              <div className="margin-left">
+  <div className="card-wrapper">
+    <img src="https://edyoda.s3.amazonaws.com/media/blog-images/learn_machine_learning_courses_online.png" class="card-img-top" alt="intro image"/>
+    <div className="card-body">
+      <h5 className="cards-headings">introduction to a machine learning online course</h5>
+      <p className="auth-wrapper"><span className="card-author"> Edyoda </span>
+        <span className="card-date">23 Aug 2019</span>
+      </p>
+      <p className="card-text"> We have had an insight into Artificial Intelligence. Well, one of its subfields is machine learning and this introduction will help you get a fair idea of what a machine learning online course holds to begin with oneize.</p>
+    </div>
+  </div>
+
+  <div className="card-wrapper">
+    <img src="https://edyoda.s3.amazonaws.com/media/blog-images/6.png" className="card-img-top" alt="intro image"/>
+    <div className="card-body">
+      <h5>LAMP Stack VS MEAN Stack</h5>
+      <p className="auth-wrapper"><span className="card-author"> Edyoda </span>
+      <span className="card-date">24 Aug 2019</span>
+      </p>
+      <p className="card-text"> In today’s dynamic world, rapid web application development with a smooth user interface that can adapt to specific features is the main business requirement. Many organizations around the world that intensely depend on programming for their everyday business.</p>
+    </div>
+  </div>
+  
+  <div className="card-wrapper">
+    <img src="https://edyoda.s3.amazonaws.com/media/blog-images/4.png" class="card-img-top" alt="intro image"/>
+    <div className="card-body">
+      <h5>Impact of Cloud Computing in various industries</h5>
+      <p className="auth-wrapper"><span className="card-author"> Edyoda </span>
+        <span className="card-date">24 Aug 2019</span>
+      </p>
+      <p className="card-text"> Cloud computing has been rapidly gaining pace in the world of information technology. It has been observed that over 90% of global enterprises are using cloud technology as part of their business structure.</p>
+    </div>
+  </div>
+  
+  <div className="card-wrapper">
+    <img src="https://edyoda.s3.amazonaws.com/media/blog-images/3.png" class="card-img-top" alt="intro image"/>
+    <div className="card-body">
+      <h5>Challenges of Machine Learning in Big Data Analytics</h5>
+      <p className="auth-wrapper"><span className="card-author"> Edyoda </span>
+        <span className="card-date">24 Aug 2019</span>
+      </p>
+      <p className="card-text">Machine Learning is a subset of artificial intelligence which is an important part of computer science.The revolution of Big Data promises to transform the way we work live and think by enabling the process of optimization.</p>
+    </div>
+  </div>
+
+  <div className="card-wrapper">
+    <img src="https://edyoda.s3.amazonaws.com/media/blog-images/5.png" className="card-img-top" alt="intro image"/>
+    <div className="card-body">
+      <h5>Breaking the Myths around Cybersecurity</h5>
+      <p className="auth-wrapper"><span className="card-author"> Edyoda </span>
+        <span className="card-date">26 Aug 2019</span>
+      </p>
+      <p className="card-text">Machine Learning is a subset of artificial intelligence which is an important part of computer science. The revolution of Big Data promises to transform the way we work, live and think by enabling the process of optimization, discovering.</p>
+    </div>
+  </div>
+
+  <div class="card-wrapper">
+    <img src="https://edyoda.s3.amazonaws.com/media/blog-images/7.png" className="card-img-top" alt="intro image"/>
+    <div className="card-body">
+      <h5>Importance of Data Backup and Recovery in the IT industry</h5>
+      <p className="auth-wrapper"><span className="card-author"> Edyoda </span>
+        <span className="card-date">26 Aug 2019</span>
+      </p>
+      <p className="card-text"> A business must always introspect the areas where they lack in order to bring about a positive change in the work environment. One of the main precautions or a contingency plan that is necessary for every business and especially industries working with clouds.</p>
+    </div>
+  </div>
+</div>
+  
+<div>
+
+<footer>
+    <div className="footer-section">
+        <h4 className="h3-head">EDYODA</h4>
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+        <a href="#">Terms of Use</a>
+        <a href="#">Privacy Policy</a>
+    </div>
+    <div className="footer-section">
+        <h4  className="h3-head">RESOURCES</h4>
+        <a href="#">Courses</a>
+        <a href="#">Learning Videos</a>
+        <a href="#">Educators</a>
+        <a href="#">Edyoda Stories</a>
+        <a href="#">University</a>
+    </div>
+    <div className="footer-section">
+        <h4 className="h3-head">FOR ENTERPRISES</h4>
+        <a href="#">Train Your Employees</a>
+        <div className = "footer-section">
+            <h4 className="h3-head">QUICK LINKS</h4>
+            <a href="#">Learn and Earn</a>
+            <a href="#">Become a Learning-Enabler</a>
+            <a href="#">Tips for Learning-Enabler</a>
+            <a href="#">Request New Topic</a>
+        </div>
+    </div>
+    <div className="footer-section">
+        <h4 className="h3-head">CONNECT</h4>
+        <a href="#">2nd Floor #188, Survey No. - 123/1,<br/>
+            Incubes Building Next to McDonalds,<br/>
+            ITPL Main Rd, Brookefield,<br/>
+            Bengaluru, Karnataka-560037,<br/>
+            India</a><br/>
+        <a href="#"><i className="far fa-envelope"></i>hello@edyoda.com</a>
+        <a href="#"><i className="fas fa-map-marker-alt"></i><i className="fab fa-twitter"></i><i className="fab fa-facebook-f"></i><i className="fab fa-linkedin-in"></i></a>
+        <a href="#">© 2021<br/>
+            zekeLabs Technologies Private Limited</a>
+    </div>
+</footer>
+
+
+</div>
+
+   </div>
+  
+
+  
+ 
+ )   
+}
+  
+
+export default App;
